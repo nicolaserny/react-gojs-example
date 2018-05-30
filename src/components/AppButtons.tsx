f577ccedcdaf210009791ea0e2f9aeb837bdd418
nicolaserny <nicolas.erny@xcomponent.com>
2018-05-30 11:09:36 +0200

    Upgrade react-gojs + other npm packages

--- src/components/AppButtons.tsx
+++ src/components/AppButtons.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
-import { DiagramState } from '../reducers/diagramReducer';
+import { DiagramState, NodeModel } from '../reducers/diagramReducer';
 import { connect, Dispatch } from 'react-redux';
 import { init, updateNodeColor, addNode } from '../actions/diagram';
+import { Action } from 'typescript-fsa';
+import { DiagramModel, LinkModel } from 'react-gojs';
 
 export interface AppButtonsDispatchProps {
     initHandler: () => void;
@@ -15,34 +17,37 @@ const mapStateToProps = (state: DiagramState) => {
     };
 };
 
-const mapDispatchToProps = (dispatch: Dispatch<DiagramState>): AppButtonsDispatchProps => {
-    let nodeId = 0;
-    return {
-        initHandler: () => dispatch(init(
-            {
-                nodeDataArray: [
-                    { key: 'Alpha', color: 'lightblue' },
-                    { key: 'Beta', color: 'orange' },
-                    { key: 'Gamma', color: 'lightgreen' },
-                    { key: 'Delta', color: 'pink' },
-                    { key: 'Omega', color: 'grey' }
-                ],
-                linkDataArray:
-                    [
-                        { from: 'Alpha', to: 'Beta' },
-                        { from: 'Alpha', to: 'Gamma' },
-                        { from: 'Beta', to: 'Delta' },
-                        { from: 'Gamma', to: 'Omega' }
-                    ]
+const mapDispatchToProps =
+    (dispatch: Dispatch<
+        Action<DiagramModel<NodeModel, LinkModel>> | Action<void> | Action<string>
+        >): AppButtonsDispatchProps => {
+        let nodeId = 0;
+        return {
+            initHandler: () => dispatch(init(
+                {
+                    nodeDataArray: [
+                        { key: 'Alpha', color: 'lightblue' },
+                        { key: 'Beta', color: 'orange' },
+                        { key: 'Gamma', color: 'lightgreen' },
+                        { key: 'Delta', color: 'pink' },
+                        { key: 'Omega', color: 'grey' }
+                    ],
+                    linkDataArray:
+                        [
+                            { from: 'Alpha', to: 'Beta' },
+                            { from: 'Alpha', to: 'Gamma' },
+                            { from: 'Beta', to: 'Delta' },
+                            { from: 'Gamma', to: 'Omega' }
+                        ]
+                }
+            )),
+            updateNodeColorHandler: () => dispatch(updateNodeColor()),
+            addNodeHandler: () => {
+                dispatch(addNode('node' + nodeId));
+                nodeId += 1;
             }
-        )),
-        updateNodeColorHandler: () => dispatch(updateNodeColor()),
-        addNodeHandler: () => {
-            dispatch(addNode('node' + nodeId));
-            nodeId += 1;
-        }
+        };
     };
-};
 
 const AppButtons = ({ initHandler, updateNodeColorHandler, addNodeHandler }: AppButtonsDispatchProps) => {
     return (
--- src/components/MyDiagramContainer.tsx
+++ src/components/MyDiagramContainer.tsx
@@ -4,6 +4,7 @@ import { DiagramState, modelSelector, NodeModel } from '../reducers/diagramReduc
 import { connect, Dispatch } from 'react-redux';
 import { nodeSelected, nodeDeselected, removeNode, removeLink } from '../actions/diagram';
 import { DiagramModel, LinkModel, ModelChangeEvent, ModelChangeEventType } from 'react-gojs';
+import { Action } from 'typescript-fsa';
 
 interface MyDiagramContainerStateProps {
     model: DiagramModel<NodeModel, LinkModel>;
@@ -20,31 +21,32 @@ const mapStateToProps = (state: DiagramState) => {
     };
 };
 
-const mapDispatchToProps = (dispatch: Dispatch<DiagramState>): MyDiagramContainerDispatchProps => {
-    return {
-        onNodeSelection: (key: string, isSelected: boolean) => {
-            if (isSelected) {
-                dispatch(nodeSelected(key));
-            } else {
-                dispatch(nodeDeselected(key));
+const mapDispatchToProps =
+    (dispatch: Dispatch<Action<string> | Action<LinkModel>>): MyDiagramContainerDispatchProps => {
+        return {
+            onNodeSelection: (key: string, isSelected: boolean) => {
+                if (isSelected) {
+                    dispatch(nodeSelected(key));
+                } else {
+                    dispatch(nodeDeselected(key));
+                }
+            },
+            onModelChange: (event: ModelChangeEvent<NodeModel, LinkModel>) => {
+                switch (event.eventType) {
+                    case (ModelChangeEventType.Remove):
+                        if (event.nodeData) {
+                            dispatch(removeNode(event.nodeData.key));
+                        }
+                        if (event.linkData) {
+                            dispatch(removeLink(event.linkData));
+                        }
+                        break;
+                    default:
+                        break;
+                }
             }
-        },
-        onModelChange: (event: ModelChangeEvent<NodeModel, LinkModel>) => {
-            switch (event.eventType) {
-                case (ModelChangeEventType.Remove):
-                    if (event.nodeData) {
-                        dispatch(removeNode(event.nodeData.key));
-                    }
-                    if (event.linkData) {
-                        dispatch(removeLink(event.linkData));
-                    }
-                    break;
-                default:
-                    break;
-            }
-        }
+        };
     };
-};
 
 const MyDiagramContainer =
     ({ model, onNodeSelection, onModelChange }: MyDiagramContainerStateProps & MyDiagramContainerDispatchProps) => {
